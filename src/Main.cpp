b7d161af95d2baccbc2b7b4a033691a01756fdc2
alcantara09 <alcantara@poli.ufrj.br>
2014-09-18 14:09:05 -0300

    Initial commit

---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <rosa_localization/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+	rosa_localization::DummyClass dummyClass;
+	dummyClass.welcome();
+
+	return 0;
+}
